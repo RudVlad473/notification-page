51fd6b269e71a0d6f570e209f8f2fed6dcd8f4a0
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-03-01 16:22:18 +0200

    initialized project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+import styles from "./App.module.scss"
+import reactLogo from "./assets/react.svg"
+
+function App() {
+  return <div></div>
+}
+
+export default App
